d05e3f8e3083cb303956d5cab97432878716adfe
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-12-28 00:22:50 +0600

    using html perfectly on javascript file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import reactDom from "react-dom";
 
 function App() {
+  var person ={
+    name: "dr. Mahfuz",
+    job: "Singer"
+  }
+
+  var person2 ={
+    name: "Eva Rahman",
+    job: "Singer"
+  }
+
+  var style ={
+    color: 'red',
+    backgroundColor:'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
+        <h3 style={style} >1. Popular Singer: {person.name + " " + person.job} </h3>
+        <h3 style={{color: 'black', background: 'yellow'}}>2. Popular Singer: {person2.name + " " + person2.job} </h3>
       </header>
     </div>
   );
